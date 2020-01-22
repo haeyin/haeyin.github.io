5f3707ce1d0607cf5d9572b3cf8ecaa5ae39a83d
Clare Shin <haeyin.shin@gmail.com>
2020-01-21 23:36:51 -0800

    Smooth Scrolling
    Dunno why smooth scrolling event was needed here, please explain.....

--- JS/CS_Portfolio.js
+++ JS/CS_Portfolio.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-        if (this.Link !=="") {
-            event.preventDefault();
-            var Link = this.Link;
-            $('html, body').animate({
-                scrollTop: $(Link).offset().top
-            }, 2000, function(){
-                window.location.Link = Link;
-            })
-        }
-    });
-});
+//$(document).ready(function(){
+//    $("a").on('click', function(event) {
+//        if (this.Link !=="") {
+//            event.preventDefault();
+//            var Link = this.Link;
+//            $('html, body').animate({
+//                scrollTop: $(Link).offset().top
+//            }, 2000, function(){
+//                window.location.Link = Link;
+//            })
+//        }
+//    });
+//});
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
